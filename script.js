5f85eaf67489d3bc20964ec2696e19bc1f327887
Sobaken27 <48553021+Sobaken27@users.noreply.github.com>
2019-11-07 15:51:07 +0300

    Commit

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
-  $(window).on('load', function () {
+new WOW().init();
+$(document).ready(function () {
+if ($(window).width() < 1679 ) {
+            $('#left1').removeClass('fadeInRight');
+            $('#left1').addClass("fadeInLeft");
+            $('#left2').removeClass('fadeInUp');
+            $('#left2').addClass("fadeInLeft");
+            $('#right1').removeClass('fadeInDown');
+            $('#right1').addClass("fadeInRight");
+            $('#right2').removeClass('fadeInLeft');
+            $('#right2').addClass("fadeInRight");
+        }
+});
+$(window).on('load', function () {
     $preloader = $('.loaderArea'),
       $loader = $preloader.find('.loader');
     $loader.fadeOut();
